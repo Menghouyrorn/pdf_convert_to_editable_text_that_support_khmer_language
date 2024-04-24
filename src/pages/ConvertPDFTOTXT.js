1b2ae69da11a5688c09e8f88d58e8bec49e52213
RORN Menghouy <menghouyrorn@gmail.com>
2024-04-24 21:54:30 +0700

    update code

--- src/pages/ConvertPDFTOTXT.js
+++ src/pages/ConvertPDFTOTXT.js
@@ -4,9 +4,11 @@ var pdfjsLib = window["pdfjs-dist/build/pdf"];
 pdfjsLib.GlobalWorkerOptions.workerSrc = "/assets/js/pdf.worker.js";
 
 const ConvertPDFTOTXT = () => {
+  const [pdffile, setPdfFile] = React.useState(null);
   const [pdfname, setPdfName] = React.useState("");
   const [text, setText] = React.useState("");
   const handleSelectFiled = (e) => {
+    setPdfFile(e.target.files[0]);
     UrlUploader(e.target.files[0]);
     setPdfName(e.target.files[0].name);
     console.log(e.target.files[0].name);
@@ -72,6 +74,17 @@ const ConvertPDFTOTXT = () => {
     document.body.appendChild(element);
     element.click();
   };
+  const sizeFile = (file) => {
+    const fileSizeInBytes = file.size; // Example file size in bytes
+    let fileSize;
+
+    if (fileSizeInBytes >= 1048576) {
+      fileSize = (fileSizeInBytes / 1048576).toFixed(2) + " MB";
+    } else {
+      fileSize = (fileSizeInBytes / 1024).toFixed(2) + " KB";
+    }
+    return fileSize;
+  };
   return (
     <div className="p-[10px]">
       <div>
@@ -89,15 +102,51 @@ const ConvertPDFTOTXT = () => {
         <div className="relative -right-[50px] z-10 -top-[60px]">
           <img src="/image/1.png" alt="" />
         </div>
-        <div className="w-[700px] bg-white shadow-md border border-spacing-1 h-[300px] z-40 mt-[60px] rounded-xl flex flex-col justify-center items-center">
-          <img src="/image/txt.png" alt="" className="w-[50px] mb-2" />
-          {text.length > 0 ? (
-            <div className="flex flex-col items-center gap-y-3">
-              <button className="w-[150px] h-[40px] flex justify-center items-center text-white text-base cursor-pointer font-bold rounded-md bg-[#1B1464] hover:shadow-md transition-all" onClick={downloadTextToFileTXT}>
-                Download
-              </button>
-            </div>
-          ) : (
+        {pdffile ? (
+          <div>
+            {text.length > 0 ? (
+              <div className="w-[700px] bg-white shadow-md border border-spacing-1 h-[300px] z-40 mt-[60px] rounded-xl flex flex-col justify-center items-center">
+                <img src="/image/pdf.png" alt="" className="w-[60px] mb-2" />
+                <div className="flex flex-col items-center gap-y-3">
+                  <div className="flex flex-col">
+                    <h1 className="text-lg">{pdfname.slice(0,-4)}.doc</h1>
+                    <p className="text-sm">{sizeFile(pdffile)}</p>
+                  </div>
+                  <button
+                    className="w-[150px] h-[40px] flex justify-center items-center text-white text-base cursor-pointer font-bold rounded-md bg-[#1B1464] hover:shadow-md transition-all"
+                    onClick={downloadTextToFileTXT}
+                  >
+                    Download
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="w-[700px] bg-white shadow-md border border-spacing-1 h-[300px] z-40 mt-[60px] rounded-xl flex flex-col justify-center items-center">
+                <div className="flex items-center w-[70%] h-[80px] rounded-md justify-between p-5 bg-[#f4f9ff] shadow-md">
+                  <img src="/image/pdf.png" alt="" className="w-[50px] mb-2" />
+                  <h1>{pdfname}.pdf</h1>
+                  <div className="flex items-center gap-x-8">
+                    <p>1 MB</p>
+                    <button className="bg-[#1B1464] w-[50px] h-[40px] text-white text-lg font-extrabold rounded-md" onClick={()=>window.location.reload()}>
+                      X
+                    </button>
+                  </div>
+                </div>
+
+                <div className="flex flex-col items-center gap-y-3 mt-[20px]">
+                  <p
+                    htmlFor="contained-button-file"
+                    className="w-[250px] h-[40px] flex justify-center items-center text-white text-base font-bold rounded-md bg-[#1B1464] hover:shadow-md transition-all"
+                  >
+                    Converter is Process...
+                  </p>
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="w-[700px] bg-white shadow-md border border-spacing-1 h-[300px] z-40 mt-[60px] rounded-xl flex flex-col justify-center items-center">
+            <img src="/image/upload.png" alt="" className="w-[60px] mb-2" />
             <div className="flex flex-col items-center gap-y-3">
               <h1 className="text-2xl text-black font-semibold">
                 Drag and Drop document here to upload
@@ -116,8 +165,8 @@ const ConvertPDFTOTXT = () => {
                 Select
               </label>
             </div>
-          )}
-        </div>
+          </div>
+        )}
         <div className="relative -left-[100px] z-20">
           <img src="/image/2.png" alt="" />
         </div>
