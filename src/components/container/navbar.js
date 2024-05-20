d761baac508035f28a388da60723fa65d7651566
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-20 18:20:56 +0700

    update code

--- src/components/container/navbar.js
+++ src/components/container/navbar.js
@@ -4,23 +4,23 @@ import { Link,useLocation } from "react-router-dom";
 function Navbar() {
   const {pathname} =useLocation();
   return (
-      <div  className="w-full h-[12vh] shadow-sm items-center flex gap-x-4 sm:gap-x-10  pl-[5px] sm:pl-[15px]">
+      <div  className="w-full h-[12vh] shadow-sm items-center flex gap-x-4 md:gap-x-32  pl-[5px] sm:pl-[15px]">
         <Link to={'/'}>
-          <div className="flex items-center gap-x-2 sm:gap-x-8 hover:cursor-pointer">
+          <div className="flex items-center gap-x-2 sm:gap-x-4 hover:cursor-pointer">
             <img src="/image/logo.png" className="w-[50px] sm:w-[70px]" alt="profile" />
             <h1 className="font-extrabold text-[#1B1464] text-sm sm:text-lg">NUM PDF COVERTER</h1>
           </div>
         </Link>
-        <div className="flex gap-x-4 items-center">
+        <div className="flex gap-x-4 items-center ">
           <Link
             to={"/"}
-            className={`w-[60px] sm:w-[90px] h-[35px] flex justify-center items-center text-lg rounded-md hover:shadow-md transition-all  ${pathname==='/' ? 'border-t-transparent border-l-transparent border-r-transparent rounded-none border-b-[3px] border-b-blue-500':'border-t-transparent border-l-transparent border-r-transparent border-b-transparent'}` }
+            className={`w-[60px] sm:w-[90px] h-[35px] flex justify-center items-center text-sm md:text-lg rounded-md hover:shadow-md transition-all  ${pathname==='/' ? 'border-t-transparent border-l-transparent border-r-transparent rounded-none border-b-[3px] border-b-blue-500':'border-t-transparent border-l-transparent border-r-transparent border-b-transparent'}` }
           >
             Home
           </Link>
           <Link
             to={"/about"}
-            className={`w-[60px] sm:w-[90px] h-[35px] flex justify-center items-center text-lg rounded-md hover:shadow-md transition-all border ${pathname==='/about' ? 'border-t-transparent border-l-transparent border-r-transparent rounded-none border-b-[3px] border-b-blue-500':'border-t-transparent border-l-transparent border-r-transparent border-b-transparent'}`}
+            className={`w-[60px] sm:w-[90px] h-[35px] flex justify-center items-center text-sm md:text-lg rounded-md hover:shadow-md transition-all border ${pathname==='/about' ? 'border-t-transparent border-l-transparent border-r-transparent rounded-none border-b-[3px] border-b-blue-500':'border-t-transparent border-l-transparent border-r-transparent border-b-transparent'}`}
           >
             About
           </Link>
